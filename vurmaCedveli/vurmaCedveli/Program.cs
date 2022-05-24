8837dd5b67ad968823b2eb6074f496c66db678bf
Sahin6033 <sahin007.sm@gmail.com>
2022-05-24 20:42:17 +0400

    initial

---
+++ vurmaCedveli/vurmaCedveli/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace TestCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        string numberString = $"{j} * {i} = {j * i} \t";
+            //        Console.Write(numberString);
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            //Console.WriteLine("Enter a number : ");
+            //byte number = Convert.ToByte( Console.ReadLine());
+            //for (int i = 1; i <= 10; i++)
+            //{
+
+
+            //     Console.WriteLine($"{number} * {i} = {number * i}");
+
+            //}
+
+            //for (int i = 1; i <=10 ; i++)
+            //{
+            //    for (int g = 1; g <= i; g++)
+            //    {
+            //        Console.Write($"{g}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+
+            #region task2
+            //Console.WriteLine("enter a number : ");
+            //int number1 = Convert.ToInt32(Console.ReadLine());
+            //int stock = 0;
+            //for (int i = 1; i <= number1; i++)
+            //{
+            //    stock += i * i;// =>  stock = stock +i*i
+
+            //}
+            //Console.WriteLine(stock);
+            #endregion
+
+
+            #region task3
+            //Console.WriteLine("Ededi daxil edin :");
+            //int number2=Convert.ToInt32(Console.ReadLine());
+            //int vurma2 = 1;
+            //int sum2 = 0;
+            //for (int i = 1; i <= number2; i++)
+            //{
+            //    vurma2 *= i;
+            //    sum2 += i;
+            //    Console.WriteLine($"{vurma2}  {sum2}");
+
+            //}
+            //Console.WriteLine($"Netice : {vurma2 - sum2}");
+
+            #endregion
+
+
+            #region verilmish ededin sade olub olmadigini yoxlanilmasi
+            //Console.WriteLine("Ededi daxil edin :");
+
+            //int eded = Convert.ToInt32(Console.ReadLine());
+            //byte counter = 0;
+            //for (int i = 2; i <= (eded/2)+1; i++) 
+            //{
+            //    if(eded%i == 0)
+            //    {
+            //        counter++;
+            //        Console.WriteLine(i);
+            //    }
+
+
+            //}
+            //if (counter == 0)
+            //{
+            //    Console.WriteLine("Sade ededdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sade eded deyil");
+            //}
+            //Console.ReadKey();
+
+
+
+            #endregion
+
+
+            #region verilmish intervalda sade edelerin  capi
+
+            Console.WriteLine("Birinci ededi daxil edin :");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin :");
+            int second = Convert.ToInt32(Console.ReadLine());
+            int ssay = 0;
+            for (int i = first; i <= second; i++){
+                bool counte = true;
+                for (int j = 2; j <= (i / 2) + 1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counte  = false;
+                        break;
+                    }
+                   
+                }
+                if (counte)
+                {
+                    Console.WriteLine(i);
+                    
+                    ssay++;
+
+                }
+            }
+            Console.WriteLine($" verilmish intervalda olan sade ededlerin sayi :  {ssay}");
+
+
+
+
+            #endregion
+
+        }
+    }
+}
